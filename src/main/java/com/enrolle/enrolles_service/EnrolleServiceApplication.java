cb828f38ea20936c1cea7779acf15c175643793b
Deepthi ch <deepthi.chemakurthi@gmail.com>
2020-10-27 16:09:32 +0530

    Git hub login
    Git hub login

--- src/main/java/com/enrolle/enrolles_service/EnrolleServiceApplication.java
+++ src/main/java/com/enrolle/enrolles_service/EnrolleServiceApplication.java
@@ -1,19 +1,46 @@
 package com.enrolle.enrolles_service;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableSwagger2
-public class EnrolleServiceApplication {
+@RestController
+@Order(1000)   
+public class EnrolleServiceApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(
+				a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated())
+				.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login().redirectionEndpoint().baseUri("/login/oauth2/code/github");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnrolleServiceApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println("================"+principal);
+		return Collections.singletonMap("name", principal.getAttribute("name"));
 	}
 
 }
